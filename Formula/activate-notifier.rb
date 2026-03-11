8419fee60c730b19ab5db786252ea3b557c3156c
Takeshi Suzuki <tockrock@gmail.com>
2026-03-12 08:44:46 +0900

    remove license for now

--- Formula/activate-notifier.rb
+++ Formula/activate-notifier.rb
@@ -3,7 +3,6 @@ class ActivateNotifier < Formula
   homepage "https://github.com/tockrock/activate-notifier"
   url "https://github.com/tockrock/activate-notifier/releases/download/v0.9.0/activate-notifier.tar.gz"
   sha256 "1f355e9c21e2f055255068fe2b6c42cdf24085ace3c2eae59fda3b4f34aa250c"
-  license "MIT"
 
   depends_on :macos => :sequoia
   depends_on "terminal-notifier"
--- Formula/terminal-bundleid.rb
+++ Formula/terminal-bundleid.rb
@@ -3,7 +3,6 @@ class TerminalBundleid < Formula
   homepage "https://github.com/tockrock/terminal-bundleid"
   url "https://github.com/tockrock/terminal-bundleid/releases/download/v0.9.0/terminal-bundleid.tar.gz"
   sha256 "6832618a871a6d8e3578d55744388d36253fda5265c88ca2a2a95c5bff6fe153"
-  license "MIT"
 
   def install
     bin.install "terminal-bundleid"
